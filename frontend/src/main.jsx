15a00aceef19605386e4fac50b6d63085bcd571c
Gonzalo Salcedo <gonzzalo996@gmail.com>
2026-07-23 22:15:53 -0500

    Creación de ruta Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
+
+
 import './index.css'
-import App from './App.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Login from './pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <browserRouter>
+  <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-  </browserRouter>
+  </BrowserRouter>
 )
